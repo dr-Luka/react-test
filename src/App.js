b7912a78c3536b65d6ab175c0ea415bc95add953
dr-Luka <63805178+dr-Luka@users.noreply.github.com>
2023-04-09 17:34:28 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./styles/style.scss";
 import Nav from "./components/Nav.js";
-// import Home from "./components/Home.js";
+import Home from "./components/Home.js";
 function App() {
   return (
     <div className="App">
       <div className="page">
         <Nav />
-        {/* <Home /> */}
+        <Home />
       </div>
     </div>
   );
